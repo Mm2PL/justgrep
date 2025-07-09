4c956060f7d93ffc569d8f43aec2b70b5e11ba51
Supa <36031171+0Supa@users.noreply.github.com>
2025-07-09 05:35:14 +0300

    Prepend timestamp and channel before message

--- cmd/irc2text/irc2text.go
+++ cmd/irc2text/irc2text.go
@@ -17,10 +17,11 @@ func main() {
 			_, _ = fmt.Fprintf(os.Stderr, "Failed to irc parse message: %s\n", err)
 			os.Exit(1)
 		}
+		fmt.Printf("[%s] %s ", msg.Timestamp.UTC().Format("2006-01-02 15:04:05"), msg.Args[0])
 		if msg.Action == "PRIVMSG" {
-			fmt.Printf("[%s] %s %s: %s\n", msg.Timestamp.UTC().Format("2006-01-02 15:04:05"), msg.Args[0], msg.User, msg.Args[1])
+			fmt.Printf("%s: %s\n", msg.User, msg.Args[1])
 		} else if msg.Action == "NOTICE" {
-			fmt.Printf("NOTICE %s %s\n", msg.Args[0], msg.Args[1])
+			fmt.Printf("NOTICE %s\n", msg.Args[1])
 		} else if msg.Action == "CLEARCHAT" {
 			if len(msg.Args) < 2 {
 				fmt.Println("Chat has been cleared")
